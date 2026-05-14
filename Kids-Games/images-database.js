3f83082b1a49caf5248e300b0541b88aba9a8ce7
Uzair Hameed <99033123+Uzair-hameed@users.noreply.github.com>
2026-05-14 19:13:05 +0500

    Create images-database.js

---
+++ Kids-Games/images-database.js
@@ -0,0 +1,359 @@
+// images-database.js
+// یہ صرف TEXT فائل ہے - کوئی تصویر اپلوڈ نہیں کرنی
+
+const URDU_IMAGES = {
+    'ا': {
+        ids: [
+            '1fkqAND0Wj0CZ-vin06nS-ANTG9yHwkC_',
+            '1msORDt3iTIy2jWVjapMNi8jhDCLTXvNk',
+            '1yZFdi1rzD4YMlxKE_Vs19KuY7vyIZuT_',
+            '1skY1eplZ1J3EgC6JG8IAgUgmekWYhjaz',
+            '11RmGlbx0zmvPiv49-iUXCzaGNAcQYEXe',
+            '1t6ihscWHSzPlWTX_jxbwRZPlZ7YBCub6'
+        ]
+    },
+    'آ': {
+        ids: [
+            '1n3M6PoMHgMmkNezrPnjVfXh-ILhB5Vbd',
+            '1MLc816ydlsQQCXmmN8NvBKrTlI8JXYBE',
+            '1nUuuAicDLjWU-aOdmCwne1RAOPu0eLxF',
+            '1CMObw5r8g2fb7qv-LQCKO9Xow4SR8YD_',
+            '1GUtj2Ktsa7P6PSgTXi4Xh8-eM4buS7Pt',
+            '1IRPiTGxlDuqD_0YaMZYTYgUmHQa0xM7D'
+        ]
+    },
+    'ب': {
+        ids: [
+            '1kylxXL09CeUR2z2vwBUTCJXu9mFAnAdX',
+            '1PSJe3OczLazIz2qohrhCRXAyWh1gdxD-',
+            '1xmXcEOpIF19C1h6kGpWeUwkv05nmlp9m',
+            '1KjJnnzK7MVnR4Y1gxDuq5FgUS7vIZgoK',
+            '195uQPWZsukNoAkVDipAdzStzStdOeAWH',
+            '1PxAGGbLyWaZORmcMBOzC1NO02gEfiD7x'
+        ]
+    },
+    'پ': {
+        ids: [
+            '1U2sIzQzUcRMkn0bPhJSlpCZ806zdQLbc',
+            '1AZA0eFqseIVKI_oZ86X4mK4Py5PnPLzg',
+            '1kF856Zq7kGk9ADLlZWeR-QmVJnf3VLSz',
+            '100GEaX1ST7fh3XrNy-O9zV0liNjTLEYd',
+            '19qLMks_EOThEtcIRtBLozpE70g9cf7An',
+            '14Ofg1G4kJdTuhEHUML3U4z7jgQWHiX6-'
+        ]
+    },
+    'ت': {
+        ids: [
+            '1koIBFdYV69LA7Q8hbWXipsQIbyyKk9bO',
+            '1Z-NOMx_OaoKeVGlgYnBu-QEoElGkybf6',
+            '1MnTDH0BzIwo34UU48Xq0vYZnLtnpTHmr',
+            '17DGLNryxH1EN6dVANlrN20yWQK2i0-Mw',
+            '1c6kNYLkxGwUTX2YhVvNxI_bpcDrskU6j',
+            '1NC365naAm8JHitPLEDeUh-_TxbNmD5G9'
+        ]
+    },
+    'ٹ': {
+        ids: [
+            '1J4XwYIyOvwFjPTCPe670-G7y6FRzftgq',
+            '1UittBNOLlrxEfvMbpW7r8X7T84qMlTGv',
+            '1YZVTb7qY6er014J5n6DhepBqX3Mu011c',
+            '11VifEs64W1Axpwwt5E4o_sJlIjBQJkqs',
+            '17ylMaYjX7vFJboJVKvRIws6FhDhVZL22',
+            '1eUvRzibJks-79mNK0Q10t2gyi0cvsFrJ'
+        ]
+    },
+    'ث': {
+        ids: ['1x0jwx6_7151_UeTigefA0mBCZT8hXgPP']
+    },
+    'ج': {
+        ids: [
+            '1RxoZz1USsk6TFrL2iwbzCzD6O9rbcFsa',
+            '11HsvqUk06h0H7vmQqEgMHdmjFNOPg_PK',
+            '1llIMwlyvf9WjWmDjxXfuD8ouRBA2mnI9',
+            '15biUB_wgBq_Fr2gu5C5N6eQq2Zb2IuXZ',
+            '1usD2b4RLTJXvdAS41h2wmZpdxjAiH4Te',
+            '1zZR_yASFBnyxhPram_tfUmlc0WOqegLQ'
+        ]
+    },
+    'چ': {
+        ids: [
+            '1DvwzvT5ZrlXToBdvxgdNVgf2BOvWzM8Z',
+            '1ggt5YNF-oGOcjJpewu2_s-OaEiIjYIPx',
+            '1iGPRmcpngUQoh4xh0IN3tG4TAsY1Hmpb',
+            '1IgX1hY_vwqUQGE95PUjKAWNt9mWoOfOf',
+            '1Mz4HxGs_2u11UScOsadkYSB9RtLLUxHL',
+            '13S5ak1Mv-Chr8QA2QljayPIaNuz43NU2'
+        ]
+    },
+    'ح': {
+        ids: [
+            '1K1NAMKMnM0Vfgt71BaEVl188lNBAI3NC',
+            '1IVrLZYcSHr_TuVFejFXMdtnbn8q8BOJH',
+            '19Dy19YLvj-a7XLSrsecWWPJBXeNrdjm-',
+            '1PsQYmNMK-p2MbyYudwx5MhqS78M7-TPA',
+            '1evXk2K2HUmGe_JFoMD5mRIjTuHdtDfh1'
+        ]
+    },
+    'خ': {
+        ids: [
+            '1ILVS9BtqRF4aNApLYwsy7LoKRIIEWwO2',
+            '1ZoEUTSpQ_BItE9RRXVdP-6H3zMPtjnPf',
+            '1F13SbGGFNl1TutYeNVBNpInW_vfp8DpA',
+            '1HKfJZ0yO2hwkQXFio54ObsJbhtVn75D8',
+            '1g6bK-QpZ_ZiPPjdFROgf6mgBX8ZQM1fs',
+            '1x5DJPCaN7Qf0Qv32KMG6AwYkV_Uepjol'
+        ]
+    },
+    'د': {
+        ids: [
+            '1005rzD1Hk88hKxvp0XI6WTKIVQr_7sk3',
+            '1Y_ENdUFryjneIyj7yZZYS4FQ8k7dzPsM',
+            '16p8diMs3MvVVbmMlwDE2nd29Y5Pyk5Tz',
+            '1-_JCuXs6uyE3zZEUitDIpsUV73P_3UlZ',
+            '1chkOG-VeKv0ucomWfPdn5sinm-15evCb'
+        ]
+    },
+    'ڈ': {
+        ids: [
+            '1jURVEHdhuGb_0nAzamP_wo_XddrxuK31',
+            '1daPOgtGd4ZADFkHEySUzygp4FvxUTTDc',
+            '1ooN-tJH2K1N1CdmWWK8vKSWnRfCKZ_3r',
+            '12Pc5hTndNzLxoib8oht3Zh5YxGv0yOq1'
+        ]
+    },
+    'ذ': {
+        ids: [
+            '1hwrD0S25T8i-uPeFQ2jZ6Ej2JOPIsmLk',
+            '1wxDQpd8JCLCHO1nuIM_gKvE6Z_FIvxNS',
+            '1kbGYXItC-zytKQ6-igwVrwT_joCoBkKj',
+            '1Ens8MROM59vQ03fQY975tBAW_fhECEGe',
+            '1Xk3pNWLLJEqHBgvSOK99ucVpyQ1UpyJX'
+        ]
+    },
+    'ر': {
+        ids: [
+            '1_N2OtIRx47kp52Tkk_CtlNiyMfpdAwjF',
+            '1tKOYs4HNekLFWCc-ndD8gcFjvJXjuZsK',
+            '1B2RAvdSXzKoChc0vanvAD3ixOmv-pqAf',
+            '1lxlToamENEmZDeDXb2zaVR8gpo6n7bb7',
+            '1TPnp4vwv5vFziQCjl2nARgS5sthDfnwg',
+            '1QE2zLPTaVNgHJ7HpYo3tRfdoHCbzDQa1'
+        ]
+    },
+    'ڑ': {
+        ids: [
+            '1nPMjNdxAxeQxl84UOtLhZsbDm8K1YLhw',
+            '1YiXgR2dmm2HL5-Ao2j39xkEnRjqCQjxd',
+            '1fr0CQIsBar1Jni0WesF9aZiCOzsKA0s3'
+        ]
+    },
+    'ز': {
+        ids: [
+            '1Yia5WCaxAEiZ0TeYTbMkqTkPvTs91fK-',
+            '1pkp6hMB1iqN06aLRgh083KK4A2ECZXhu',
+            '1VDgmm94Lpkx-RDkyR-OTDBitAPt8rMUt',
+            '1nKqLGVAhK5jbff0Ra5G8JDX5tBk4jrOs',
+            '1sFEIY_ZX2ev1qqu1idysW1eSOIPEZ2FE',
+            '1cVNyIjEAiCzHav3U6LsYX92_AQSeblE8'
+        ]
+    },
+    'س': {
+        ids: [
+            '1BGBfqVuJb7xtT2GUhgMQU1zk2E13AOC2',
+            '179OP4-ZOzV8FaKzS4FkBqrGOasIaVcRd',
+            '1RGc2Sa3vbtO8E1cycviGo5t9eYdtFOah',
+            '1RnWC2d3ulJSfcRzgiTz9_EsFH-i0vxsS',
+            '1DmXWzFK4zm9FD6BxmQXHU62trCWBVUup',
+            '15kx1G-auwzK2bUGZPm5Q1h3Vb0lowQja'
+        ]
+    },
+    'ش': {
+        ids: [
+            '1wUJMd5LQR1rqK7GsE-NkGZePVqz9Zg17',
+            '1f7l-KHjsYKtxRHwnCj4cMY0kbh5EZ1Di',
+            '17KP_K5O0mzlNELnft6MyUS8sB_khPEIb',
+            '1MpDHkdmwjjGN5FzCmnaXUU8u5JdVr8C3',
+            '1KVqUtmT-bfBlmRK743FAozIW2srV3Sg3',
+            '1YyAYXGDKrSR2bvDR-3mBD1OY0spPhNjr'
+        ]
+    },
+    'ص': {
+        ids: [
+            '18pQ2i_5GQoDh2RnMg9_xTNrkanAUT5NB',
+            '1RbSYF8fcf_wUw07mOMTMCyzG1nQXhU5b',
+            '1uUChh93LIGdigADWVoOKFgYLU62Yof_g',
+            '1TpnJoj4-5RfXwZgDNem5J9i6fcIiS7nv',
+            '1s7Pd3qHrjnq3ob07YM3O8z84zLttH0ml',
+            '1A93NLA3_EI_jUCPVyj5mEodXPackbSdl'
+        ]
+    },
+    'ض': {
+        ids: [
+            '1OQ0Wc6lLOpCt5N6f2VZgCobT3KhlNZTe',
+            '1J0sRpd66csg53Q5JGR-0YWdeltWkVKnt',
+            '1tq9qZ5qmfhNj_gTm_SH6L59JDvoMUfX0',
+            '1Ziy0D5zVmN6vhpjIBVzv4PNQ8nG14kyb'
+        ]
+    },
+    'ط': {
+        ids: [
+            '1Usyaszm_W_F0-nGPIcMJYawDGzj2wTkL',
+            '1TdMQB88snHYqyOJtethYmyL87rtDzq1W',
+            '15N-bVFRwVx8kEiXkw39wVDAhbId2h96A',
+            '1Kicp1ii5wc9ZJDg3lDrQErDOw_qi5Dwa',
+            '14YRqAFJCGRfmIKOJDuJFPl5DyPheTN0C',
+            '1BqJvfxVzppJxc3qXXfM78VjCeETpGN2M'
+        ]
+    },
+    'ظ': {
+        ids: [
+            '1OULQpu0kA8aUZCcs3kImlHTF38dNrAWM',
+            '18Cj9yIwJEXDgzGMGEj82g-Pl8ANjITIB',
+            '1utPzNp8qbzN74A4JH-rWMkIngPcAqn6X'
+        ]
+    },
+    'ع': {
+        ids: [
+            '1qPiC5aGvSr59A_HUe1bmtdQUwnGyL7Am',
+            '18AUmhEV6KKeRJO_d6SZHFA9qTBwkAvjB',
+            '1nZSgIWoseVM5M3yU3q4pf-LHV0Ncvn1n',
+            '19MJXcqywk1ecYi4ftMg2TDWh6Dw43TKS',
+            '1SHvuCkDRkNkN4dHlOJoaDLzYfUCUa1EG',
+            '1F-bf-nm1J4t9NsVCf4waP_Djj0dkPynN'
+        ]
+    },
+    'غ': {
+        ids: [
+            '1nkstxswRZj9FHTluYVQGzeg5X10_CLgH',
+            '1Ucn4lLa8K9z7ojj7498GlaHaI56i0nTH',
+            '11BmVQ6NFvWRURSVTkiu3UHYR9p6sgOws',
+            '189GFa1Y7_3-PeWGQYalGk1YiuoMzG5dl',
+            '1O7qugSkRWO_dEaIEtqwlHTE0GLqylzGz',
+            '1GvAGf_dGZGTQeMCXPUSB5GU0vw0a4Zox'
+        ]
+    },
+    'ف': {
+        ids: [
+            '1SEyOHC_Z7RNwwDejAl-CZj5o95eeERnk',
+            '1e2r_rVNd8EbrtwK0Qgt04sNQI1VHr6mM',
+            '1Q633UiT3x30cZpfh4knlRp2EwYohmheF',
+            '1yndSkpCzqVoBcPBGQuNvswjT5f0loN5h',
+            '15nF4hKK-W8M25uaqC6OcNWoFDGC3XL0e',
+            '17zT2v1rusHo6c8fPFEoiivcp4W_AbzW9'
+        ]
+    },
+    'ق': {
+        ids: [
+            '1WFPLaSIQps9PSO4LkQsSIoMzixLbD3tI',
+            '1EYGSFq9Y5AB7qCG1xnbQmJK0IyjasAxQ',
+            '1RbgqqmkDJ7UiH3v2VMwQq6BdWxOsePC3',
+            '1yxQixH7ruifqB-5EwmbR0jHHGcgvm57a',
+            '1nm88JKkFPVjdJ_h4TCr0G8wD6yqQVgTFe'
+        ]
+    },
+    'ک': {
+        ids: [
+            '1dncMYa-Ox8T0Gk7tNfzgbMtLjBjbkucp',
+            '1c31h1f4q8Ku_rNS0aqfyfWbl9aXeS2Wh',
+            '1OfTuzfsyK12Jpx_beAz7JEUXgXnf5uXO',
+            '1fs4BPhei7PPQO2PxjGvpraKouUoSB960',
+            '1KIQ55LEmJ1qina2KytzenKT6p2_yhxEr',
+            '1vmcT04uKD14KxNcZ40Cgu9OhJpPQ7wUG'
+        ]
+    },
+    'گ': {
+        ids: [
+            '1OWWqzTpi4Bp2m1huGZDJjO-YAcHBnfAu',
+            '1_9B6U9qjHxttAVZvQ9ISRgvAz76duonO',
+            '1q1eZ1Yb1FoX_loE-YxoT_Hi-eqpM7qKA',
+            '1zBhNTGfRdkmrCbQ5Gs9exHhLuDZ5sN5p',
+            '1sBai-PjHCdCqlh2jOHqwUgzVmMp4D-cJ',
+            '1RvGCrPZa8mIyamsTovuVBWX5r_yyzIHf'
+        ]
+    },
+    'ل': {
+        ids: [
+            '1bftsVJJujHe_5pgAavymJqusHreg8Tb8',
+            '1qKunJ3GSDRoL_cz7DlBb4uaW4fPLTFKh',
+            '1oH7N_qI3Oe7M-AvYrxovEn8H6iAydvz3',
+            '1ysuFivfW38gYvb9aY7Kh7HjPfzTZtK68',
+            '12NO44mEQAgNyRNKXhcDrx7uYSje9JYao',
+            '1duPoyfADjUa1HnxE63q2NC8U1Z3lhXgW'
+        ]
+    },
+    'م': {
+        ids: [
+            '1BAeBBWGJzpU4EK0VxuTUYCN-insEhVyB',
+            '1foGRhSNFqoF4NPlWA1NyhdnomQc2SY7c',
+            '1mfrQ6toeK-Gca08AdsMUBHIayZCzMpsW',
+            '10G9jcpEDsWMNjgumLiE0ScG3K8n2tkrK',
+            '1_ZLWe07rb7LlGblQLLOm_DDohtqBBhVc',
+            '1GecDeYbvm9qEtcqi8FxPjuBWO7tG7OEN'
+        ]
+    },
+    'ن': {
+        ids: [
+            '1xG4NLys54lNLYO8vdTDJf1BDwcDf7NaJ',
+            '1JzeI25hzugfWYn3T_F54GcEK2Oe_Soco',
+            '1LdOaIy7IgiA1tclkSzK47rr76Bqyl8FD',
+            '1SxX0BCwPMynjMS_Vjo7l0hRNudRsXyES',
+            '1mM3jHLPoepmDzVYyIz1GG0IVEN3LL9ns',
+            '1tIqpXfZIB5-5Xie90zurCk09p203j8Xg'
+        ]
+    },
+    'و': {
+        ids: [
+            '1nLc0dnqvPOE0tvo2LUMGkd1K9bTx-PWU',
+            '1mwIPBT_ldRm-jbotmAwmnBR3i7YEiQiB',
+            '1hB-biOVfjRCnwirGwWdwaxSXelqwl5B7',
+            '1OQsycvWzHHo_ZpXzhJsdZ2hKHG4U1uDu',
+            '1pU-LsgiCbfddiyJWsPQ2FH2YM8DicFvE',
+            '1daZmJxSHx7BHG2fxumQhbMIPx7p26ONr'
+        ]
+    },
+    'ہ': {
+        ids: [
+            '1yThdbF0QIxQV6mhRWG_oR6zAjKts4aXk',
+            '1naD0DJQdBuA_FT4u5fZb99lnk9q3bH2Z',
+            '1vvNnJfmOR21sPqDb8R54hkf0a0V7nzEW',
+            '1xXh8_XIm0Kv2efAeP8dGzJyhYCuSW-Rc',
+            '1F5MEhwGW_HdY8caPUdiLXw8o_5E4x2Q1',
+            '1wCyYHEwFpXmvBMd8IPNPzOWm-Mkp8z-'
+        ]
+    },
+    'ء': {
+        ids: [
+            '1Ch6G8Q01soUy1NladS7rjMlsO3tckvGD',
+            '1PqycHafOuSsZXXIeCndwoeZ6K1F-bOgq',
+            '13vtGg36N4NH62dkZ2U8PBy7vIhvSh5WP'
+        ]
+    },
+    'ی': {
+        ids: [
+            '1x8N8Rg8i4-QwGQSgD4S8HSIOFKxm94u_',
+            '1b6u115NUxGSDrZ5OuDsR3pzUocDbydhA',
+            '1crADm-lQIFdM743nuM8dMwPXajYhIXJI',
+            '1vwlLaUsf3z4s-LHV27tTA2oJAdanCrFn',
+            '1VgbKGs0M585Pgg0Rqj3wo0TwX_nyk2oK'
+        ]
+    },
+    'ے': {
+        ids: [
+            '1x8N8Rg8i4-QwGQSgD4S8HSIOFKxm94u_',
+            '1b6u115NUxGSDrZ5OuDsR3pzUocDbydhA',
+            '1crADm-lQIFdM743nuM8dMwPXajYhIXJI'
+        ]
+    }
+};
+
+// Helper function
+function getImageUrl(imageId) {
+    return `https://drive.google.com/uc?export=download&id=${imageId}`;
+}
+
+function getRandomImageForLetter(letter) {
+    const images = URDU_IMAGES[letter]?.ids;
+    if(!images || images.length === 0) return null;
+    const randomId = images[Math.floor(Math.random() * images.length)];
+    return getImageUrl(randomId);
+}
